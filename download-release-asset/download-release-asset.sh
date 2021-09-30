57764b913143e70ebcbb594f6198695ca7df5780
O. El Hosami <nosamad@users.noreply.github.com>
2021-09-30 14:28:50 +0200

    Fix shellcheck warnings

--- download-release-asset/download-release-asset.sh
+++ download-release-asset/download-release-asset.sh
@@ -6,6 +6,7 @@ set -o pipefail
 GITHUB_API_URL="${GITHUB_API_URL:-https://api.github.com}"
 DOWNLOAD_ASSET_REPO="${DOWNLOAD_ASSET_REPO:-${GITHUB_REPOSITORY}}"
 DOWNLOAD_ASSET_TAG="${DOWNLOAD_ASSET_TAG:-latest}"
+DOWNLOAD_ASSET_URLFILTERPATTERN="${DOWNLOAD_ASSET_URLFILTERPATTERN:-.*}"
 
 function curl() {
   echo "curl " "$@" 1>&2
@@ -14,32 +15,31 @@ function curl() {
     "$@"
 }
 
-if [[ -z "$DOWNLOAD_ASSET_REPO" ]]; then
-  echo "$0:error: DOWNLOAD_ASSET_REPO empty" 1>&2
-  exit 1
-fi
-if [[ -z "$DOWNLOAD_ASSET_TOKEN" ]]; then
-  echo "$0:error: DOWNLOAD_ASSET_TOKEN empty" 1>&2
-  exit 1
-fi
+function main() {
+  if [[ -z "$DOWNLOAD_ASSET_REPO" ]]; then
+    echo "$0:error: DOWNLOAD_ASSET_REPO empty" 1>&2
+    exit 1
+  fi
+  if [[ -z "$DOWNLOAD_ASSET_TOKEN" ]]; then
+    echo "$0:error: DOWNLOAD_ASSET_TOKEN empty" 1>&2
+    exit 1
+  fi
 
-jq_asset_filter=""
-if [[ "${DOWNLOAD_ASSET_TAG}" != 'latest' ]]; then
+  jq_asset_filter=""
+  if [[ "${DOWNLOAD_ASSET_TAG}" != 'latest' ]]; then
     jq_asset_filter=". | map(select(.tag_name == \"${DOWNLOAD_ASSET_TAG}\"))[0].assets[]"
-else
+  else
     jq_asset_filter='.[0].assets[]'
-fi
+  fi
 
-asset_urls=(
-    $(curl \
+  readarray -t asset_urls < <(curl \
       --url "${GITHUB_API_URL}/repos/${DOWNLOAD_ASSET_REPO}/releases" \
       --header 'Accept: application/json' \
       --fail \
-    | jq -r "${jq_asset_filter} | .browser_download_url | select(contains(\"${DOWNLOAD_ASSET_URL_FILTER}\"))" \
-    )
-)
-
-for url in "${asset_urls[@]}"; do
+    | jq -r "${jq_asset_filter} | .browser_download_url | select(.|test(\"${DOWNLOAD_ASSET_URLFILTERPATTERN}\"))" \
+  )
+  
+  for url in "${asset_urls[@]}"; do
     filename="$(basename "${url}")"
     curl \
       --url "${url}" \
@@ -47,4 +47,10 @@ for url in "${asset_urls[@]}"; do
       --fail \
       --output "${filename}"
     echo "${PWD}/${filename}"
-done
+  done
+}
+
+(return 0 2>/dev/null) && sourced=1 || sourced=0
+if [[ "${sourced}" -ne 1 ]]; then
+  main "$@"
+fi
